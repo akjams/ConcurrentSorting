903bea7990dc1fd0d0e44048dc945f87f7bb2379
Austin Kemper <austin.kemper@live.com>
2017-01-26 17:57:07 -0500

    configure junit4 and add test case

--- src/test/java/com/austinkemper/concurrentsorting/SortCorrectnessTest.java
+++ src/test/java/com/austinkemper/concurrentsorting/SortCorrectnessTest.java
@@ -5,10 +5,35 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class SortCorrectnessTest {
+    
+    private final int TEST_SIZE = 1000;
+    
+    private boolean isSorted(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private int[] getReverseSortedArray() {
+        int[] array = new int[TEST_SIZE];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = TEST_SIZE - i;
+        }
+        return array;
+    }
+    
+    private boolean sorterWorks(Sorter sorter) {
+        int[] array = getReverseSortedArray();
+        sorter.sort(array);
+        return isSorted(array);
+    }
 
     @Test
-    public void test() {
-        //fail("Not yet implemented");
+    public void testSimpleSorter() {
+        assertTrue(sorterWorks(new SimpleSorter()));
     }
 
 }
